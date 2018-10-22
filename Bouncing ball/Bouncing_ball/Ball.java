21b650b5735c283db7eb2e0103867e87fcbfbb3b
Melvin Tran <m.tran14@EDU.epsb.ca>
2018-10-22 11:53:12 -0600

    yeated

--- Bouncing ball/Bouncing_ball/Ball.java
+++ Bouncing ball/Bouncing_ball/Ball.java
@@ -1,25 +1,41 @@
 class Ball {
-  int x = 250;
-  int y = 250;
+  private int x = 250;
+  private int y = 250;
+  private int diameter;
 
-  int targetX = 250;
-  int targetY = 250;
+  public int targetX = 250;
+  public int targetY = 250;
+
+  Ball(int x, int y, int diameter) {
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    targetX = x; //starting value must match initial instance value
+    targetY = y; //starting value must match initial instance value
+  } //End of Constructor
+
 
   void step() {
+
+
     if (x < targetX) {
       x++;
     } else {
       x--;
     }
-    if (y < targetY){
-    y++;
+    if (y < targetY) {
+      y++;
     } else {
-    y--;
+      y--;
     }
   }
-
-  Ball(int x, int y) {
-    this.x = x;
-    this.y = y;
+  public int getX() { 
+    return x;
+  }
+  public int getY() {
+    return y;
+  }
+  public int getDiameter() {
+    return diameter;
   }
 }
--- Bouncing ball/Bouncing_ball/Bouncing_ball.pde
+++ Bouncing ball/Bouncing_ball/Bouncing_ball.pde
@@ -1,19 +1,21 @@
-Ball ball = new Ball(250,250);
-void setup(){
-size(500,500);
-ellipseMode(CENTER);
+Ball ball = new Ball(250, 250, 25);
+Boolean start = false;
+void setup() {
+  size(500, 501);
+  ellipseMode(CENTER);
+  ScreenSizeChecker();
 }
-
-void draw(){
-  
-  ball.step();
-  
-background(0);
-ellipse(ball.x,ball.y, 4500, 450);
+void draw() {
+  startStop();
+  if (start ==  true) {
+    ball.step();
+    background(0);
+    ellipse(ball.getX(), ball.getY(), ball.getDiameter(), ball.getDiameter());
+  }
 }
-
-void mouseClicked(){
-ball.targetX = mouseX;
-ball.targetY = mouseY;
-
+void mouseClicked() {
+  ball.targetX = mouseX;
+  ball.targetY = mouseY;
+  //ball.X = mouseX;
+  //ball.Y = mouseY;
 }
